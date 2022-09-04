153405b7fcb10649da252071dc7d85a0cb0573dd
Adam Tkaczyk <adam@tkaczyk.dev>
2022-09-04 12:51:09 +0200

    Add better logging

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -78,6 +78,7 @@ export async function createServer() {
           where: { id: parseInt(userId) },
           include: { team: true },
         })
+        req.log.info(user)
       }
 
       return {
