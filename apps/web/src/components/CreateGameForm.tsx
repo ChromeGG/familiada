c866f62575d9ef6cbd41ec5195b6877a96329f8d
Adam Tkaczyk <adam@tkaczyk.dev>
2022-09-16 22:18:23 +0200

    Detect error with TS

--- apps/web/src/components/CreateGameForm.tsx
+++ apps/web/src/components/CreateGameForm.tsx
@@ -2,19 +2,12 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Button } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import { FormContainer, TextFieldElement } from 'react-hook-form-mui'
-import type { infer as Infer } from 'zod'
+
 import { z } from 'zod'
 
 const schema = z.object({
-  name: z
-    .string()
-    .min(3)
-    .max(15)
-    // replace by validator.js?
-    .regex(/[A-Za-z0-9_]/),
+  name: z.string().min(3).max(15),
 })
-
-type Schema = Infer<typeof schema>
 
 const CreateGameForm = () => {
   // const createGameForm = useCreateGameForm()
@@ -23,18 +16,18 @@ const CreateGameForm = () => {
     console.log(data)
   }
 
-  const createGameForm = useForm<Schema>({
-    // @ts-ignore: I'll fix it later
-    resolver: zodResolver(schema),
-  })
+  // ! This causes the error :(
+  // const createGameForm = useForm({
+  //   resolver: zodResolver(schema),
+  // })
 
   return (
     <FormContainer
-      formContext={createGameForm}
-      handleSubmit={createGameForm.handleSubmit(
-        async (data) => await inviteToMyHousehold(data)
-        // async (data) => await inviteToMyHousehold(data)
-      )}
+    // formContext={createGameForm}
+    // handleSubmit={createGameForm.handleSubmit(
+    //   async (data) => await inviteToMyHousehold(data)
+    //   // async (data) => await inviteToMyHousehold(data)
+    // )}
     >
       <TextFieldElement name="name" label="lol" />
       <Button type="submit" variant="contained" fullWidth>
