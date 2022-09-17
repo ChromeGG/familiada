683fe36996bd4a8d6efc4725037fc7ee9af08a7f
Adam Tkaczyk <adam@tkaczyk.dev>
2022-09-17 22:21:30 +0200

    Show issue with infinite Type

--- apps/web/src/components/CreateGameForm.tsx
+++ apps/web/src/components/CreateGameForm.tsx
@@ -3,16 +3,14 @@ import { Button } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import { FormContainer, TextFieldElement } from 'react-hook-form-mui'
 
-import { z } from 'zod'
+import * as z from 'zod'
 
 const schema = z.object({
   name: z.string().min(3).max(15),
 })
 
 const CreateGameForm = () => {
-  // const createGameForm = useCreateGameForm()
-
-  const inviteToMyHousehold = async (data: any) => {
+  const createGameHandler = async (data: any) => {
     console.log(data)
   }
 
@@ -25,8 +23,7 @@ const CreateGameForm = () => {
     <FormContainer
     // formContext={createGameForm}
     // handleSubmit={createGameForm.handleSubmit(
-    //   async (data) => await inviteToMyHousehold(data)
-    //   // async (data) => await inviteToMyHousehold(data)
+    //   async (data) => await createGameHandler(data)
     // )}
     >
       <TextFieldElement name="name" label="lol" />
