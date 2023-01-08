7fb02fff9024c89c51292cedcef329afe1fc19c5
Adam Tkaczyk <adam@tkaczyk.dev>
2023-01-08 21:39:31 +0100

    build: :construction_worker: try to build docker image for railway

---
+++ apps/api/scratch.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const server = http.createServer()
+
+server
+  .on('request', (request, response) => {
+    let body = []
+    request
+      .on('data', (chunk) => {
+        body.push(chunk)
+      })
+      .on('end', () => {
+        body = Buffer.concat(body).toString()
+
+        console.log(`==== ${request.method} ${request.url}`)
+        console.log('> Headers')
+        console.log(request.headers)
+
+        console.log('> Body')
+        console.log(body)
+        response.end()
+      })
+  })
+  .listen(8083)
+console.log('Listening on port 8083');
