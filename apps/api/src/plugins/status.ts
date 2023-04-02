98ff8cc6c7c1db12bc6dd4e763d9bbd9a5dac1b0
Adam Tkaczyk <adam@tkaczyk.dev>
2023-04-02 09:56:39 +0200

    Add dockerfile for API

--- apps/api/src/plugins/status.ts
+++ apps/api/src/plugins/status.ts
@@ -3,6 +3,7 @@ import type { FastifyPluginAsync } from 'fastify'
 import { prisma } from '../prisma'
 
 const statusPlugin: FastifyPluginAsync = async (server, _options) => {
+  // TODO the errors is not captured by the error handler
   server.get(`/`, async function (_req, _res) {
     await prisma.$queryRaw`SELECT 1`
     return { up: true }
