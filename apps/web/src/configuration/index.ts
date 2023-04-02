cef59a1cda4f31ff48237da2dcfd683ac0590af2
Adam Tkaczyk <adam@tkaczyk.dev>
2023-04-02 11:31:45 +0200

    Set ENV before running the instal scripts

--- apps/web/src/configuration/index.ts
+++ apps/web/src/configuration/index.ts
@@ -1,5 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production'
 
 export const config = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL, //  '//process.env.API_URL,
+  apiUrl: process.env.NEXT_PUBLIC_API_URL,
 }
