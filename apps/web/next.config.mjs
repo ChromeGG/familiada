98ebeaae05ca4f119743d0d431d8a26ac10b4cce
Adam Tkaczyk <adam@tkaczyk.dev>
2023-04-02 13:26:13 +0200

    Add nextjs headers

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,8 @@ import withTM from 'next-transpile-modules'
 import { createSecureHeaders } from 'next-secure-headers'
 import nextTranslate from 'next-translate'
 
+// console.log(process.env.NEXT_PUBLIC_API_URL)
+// const apiUrl = process.env.NEXT_PUBLIC_API_URL
 /**
  * @type {import('next').NextConfig}
  */
@@ -11,10 +13,23 @@ const nextConfig = {
   poweredByHeader: false,
   async headers() {
     return [
-      { key: "Access-Control-Allow-Credentials", value: "true" },
-      { key: "Access-Control-Allow-Origin", value: "*" },
-      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+      {
+        // matching all API routes
+        source: '/api/(.*)',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
     ]
   },
 }
