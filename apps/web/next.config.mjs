4c5a8eb17aa07f1341c447e4323828a087e4cbd2
Adam Tkaczyk <adam@tkaczyk.dev>
2023-04-02 13:16:47 +0200

    Add headers manually

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,9 +9,14 @@ const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   poweredByHeader: false,
-  // async headers() {
-  //   return [{ source: '/(.*)', headers: createSecureHeaders() }]
-  // },
+  async headers() {
+    return [
+      { key: "Access-Control-Allow-Credentials", value: "true" },
+      { key: "Access-Control-Allow-Origin", value: "*" },
+      { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+      { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    ]
+  },
 }
 
 export default withTM([])(nextTranslate(nextConfig))
