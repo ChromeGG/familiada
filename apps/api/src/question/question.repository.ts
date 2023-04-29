9e0f1a2bc67ac6c6f52494cb7cd4f5bad2ba8829
Adam Tkaczyk <adam@tkaczyk.dev>
2023-04-29 17:42:11 +0200

    feat(question): :sparkles: obtain random question

--- apps/api/src/question/question.repository.ts
+++ apps/api/src/question/question.repository.ts
@@ -2,15 +2,21 @@ import type { Language, Question } from '@prisma/client'
 
 import { prisma } from '../prisma'
 
+const getRandomElement = <T>(array: T[]): T => {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
 export const gameRepository = {
   getRandomQuestion: async (
     language: Language,
     skipIds: Question['id'][] = []
   ) => {
-    // TODO make it random
-    return prisma.question.findFirstOrThrow({
+    // TODO get random question using SQL instead over-fetching all questions
+    const questions = await prisma.question.findMany({
       where: { language, id: { notIn: skipIds } },
       orderBy: { id: 'asc' },
     })
+
+    return getRandomElement(questions)
   },
 }
